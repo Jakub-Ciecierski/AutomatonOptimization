799a2d2956770ca8af699a1d6ad5f3c1d5ea0c71
Jakub-Ciecierski <jakub.ciecier@gmail.com>
2015-10-30 19:19:52 +0100

    fixed doc

--- automata/src/main.cpp
+++ automata/src/main.cpp
@@ -6,8 +6,7 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-   if(isBig(101))
-      printf("mmm so big\n");
-
-   return EXIT_SUCCESS;
+    if (isBig(101))
+        printf("mmm so big\n");
+    return EXIT_SUCCESS;
 }
--- automata/test/src/test.cpp
+++ automata/test/src/test.cpp
@@ -4,21 +4,21 @@
 #include "console.cpp"
 
 TEST(MathTest, TwoPlusTwoEqualsFour) {
-	EXPECT_EQ(2 + 2, 4);
+    EXPECT_EQ(2 + 2, 4);
 }
 
 TEST(MathTest2, TwoPlusTwoEqualsFour2) {
-	EXPECT_EQ(2 + 2, 4);
+    EXPECT_EQ(2 + 2, 4);
 }
 
 TEST(MyTest, isBigTrue) {
-	int number = 101;
+    int number = 101;
 
-	EXPECT_EQ(isBig(number), true);
+    EXPECT_EQ(isBig(number), true);
 }
 
 TEST(MyTest, isBigFalse) {
-	int number = 99;
+    int number = 99;
 
-	EXPECT_EQ(isBig(number), false);
+    EXPECT_EQ(isBig(number), false);
 }

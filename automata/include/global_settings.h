ab86e7eedd28db87b870a73334747d63dae64662
dybisz <bartek.dybisz@gmail.com>
2015-11-13 18:42:02 +0100

    global variables set

--- automata/include/global_settings.h
+++ automata/include/global_settings.h
@@ -10,11 +10,39 @@
 // Few remarks about whole project:
 // |--o   symbols take values from interval [1; _numberOfSymbols]
 // |--o   states are marked by values from interval [1; _numberOfStates]
+// |--o   actual values declared in global_settings.cpp (<3 c++)
 
 namespace global_settings {
+
+    /* ------ PSO SETTINGS ----- */
+    // URL to file with tool automaton
     extern std::string TOOL_URL;
+    // Minimum states to check
     extern int MIN_STATES;
+    // Maximum number of states to check
     extern int MAX_STATES;
+
+    /* ------ WORDS GENERATION ----- */
+    // Maximum number of testing words
+    extern int R_MAX;
+    // Number of words in omega_{S}
+    extern int SIZE_S;
+    // Number of words in omega_{M}
+    extern int SIZE_M;
+    // Number of words in omega_{L}
+    extern int SIZE_L;
+    // Minimum word length in omega_{S}
+    extern int MIN_LENG_S;
+    // Maximum word length in omega_{S}
+    extern int MAX_LENG_S;
+    // Minimum word length in omega_{M}
+    extern int MIN_LENG_M;
+    // Maximum word length in omega_{M}
+    extern int MAX_LENG_M;
+    // Minimum word length in omega_{L}
+    extern int MIN_LENG_L;
+    // Maximum word length in omega_{L}
+    extern int MAX_LENG_L;
 }
 
 
--- automata/src/global_settings.cpp
+++ automata/src/global_settings.cpp
@@ -5,7 +5,20 @@
 #include "global_settings.h"
 
 namespace global_settings {
-    std::string TOOL_URL = "test_jastrz.txt";
-    int MIN_STATES = 1;
-    int MAX_STATES = 6;
+    /* ------ PSO SETTINGS ----- */
+    std::string TOOL_URL    = "test_jastrz.txt";
+    int MIN_STATES          = 1;
+    int MAX_STATES          = 6;
+
+    /* ------ WORDS GENERATION ----- */
+    int R_MAX               = 30;
+    int SIZE_S              = 10;
+    int SIZE_M              = 10;
+    int SIZE_L              = 10;
+    int MIN_LENG_S          = 1;
+    int MAX_LENG_S          = 5;
+    int MIN_LENG_M          = 6;
+    int MAX_LENG_M          = 15;
+    int MIN_LENG_L          = 16;
+    int MAX_LENG_L          = 25;
 }

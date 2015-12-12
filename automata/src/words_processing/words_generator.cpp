c40b84c04869b7d557d4859a911f1dcc730c8db7
dybisz <bartek.dybisz@gmail.com>
2015-12-12 14:24:27 +0100

    words gen changes 1

--- automata/src/words_processing/words_generator.cpp
+++ automata/src/words_processing/words_generator.cpp
@@ -4,105 +4,231 @@
 
 #include <logger/log.h>
 #include "words_generator.h"
-
-WordsGenerator::WordsGenerator(vector<int> alphabet) : _alphabet(alphabet),
-                                                       _omegaS(MIN_LENG_S, MAX_LENG_S),
-                                                       _omegaM(MIN_LENG_M, MAX_LENG_M),
-                                                       _omegaL(MIN_LENG_L, MAX_LENG_L),
-                                                       _testSet(MIN_LENG_S, MAX_LENG_L) {
+WordsGenerator::WordsGenerator(vector<int> alphabet) {}
+WordsGenerator::WordsGenerator(string url) {
+  _loadWordsFromFile(url);
+}
+WordsGenerator::WordsGenerator(vector<int> alphabet, int c, int trainSet) : _alphabet(alphabet),
+                                                       _C(c),
+                                                       _wordsInTrainingSet(trainSet),
+                                                       _maxWordLengthTraining(c + 10),
+                                                       _wordsInTestingSet(3001),
+                                                       _maxWordLengthTesting(c + 20) {
+                                                          //TODO hwdp
     try {
         utils::seed();
-        //_checkGlobalConditions();
-        //_fillBags();
-		_fillTestBag(SIZE_S);
-        //_createAllWordsUpToLength(R_MAX);
-        printAllKLength(_alphabet, R_MAX);
-        _generatePairs();
-        _generateTestPairs();
-        
-        
-		std::stringstream ss;
-		ss << "Words Generated:" << std::endl;
-		ss << "All up to length:   " << R_MAX << std::endl;
-		ss << "Number of words:    " << _psoWords.size() << endl;
-		ss << "Size Test:    	   " << SIZE_S;
-		logger::log(File("words.txt"), ss.str());
-        
+        _calculateNumberOfWords();
+        _checkGlobalConditions();
+        _generateWords();
+        _saveWordsToFIle();
+        _printInfo();
     }
-    catch (std::exception &e) {
+        catch (std::exception &e) {
         LOG_ERROR(e.what())
     }
 }
 
+void WordsGenerator::_loadWordsFromFile(string url) {
+    cout << "Opening file: " << url << endl;
+    std::ifstream infile(url);
+    string line;
+
+    // HEADER PART
+    _loadHeader(infile);
+
+    //ALPHABET
+    std::getline(infile, line);
+    istringstream iss(line);
+    int alphabet;
+    iss >> alphabet;
+    _createAlphabet(alphabet);
+
+    // SKIP shit
+    _loadHeader(infile);
+
+    // FIRST subset
+    _firstSubset = _parseWords(infile);
+
+    // SECOND subset
+    _secondSubset = _parseWords(infile);
+
+    // TESTING set
+    _testingSet = _parseWords(infile);
+}
+void WordsGenerator::_loadHeader(ifstream & infile) {
+  string line;
+  while (std::getline(infile, line))
+  {
+    istringstream iss(line);
+    char  firstSymbol;
+    if (!(iss >> firstSymbol)) { break; } // error
+    if(firstSymbol == '[') break;
+  }
+  std::getline(infile, line);
+}
+void WordsGenerator::_createAlphabet(int n) {
+    for(int i = 0; i < n ; i++) {
+      _alphabet.push_back(i);
+    }
+    cout << "Alphabet loaded: " << utils::vectorToString(_alphabet) << endl;
+}
+
+vector<Word> WordsGenerator::_parseWords(ifstream & infile) {
+  string line;
+  vector<Word> words;
+
+
+  while (infile)
+  {
+    string s;
+    if (!getline( infile, s )) break;
+    if(s[0] == '[') break;
+
+    istringstream ss( s );
+    vector <string> record;
+    Word _word;
+    while (ss)
+    {
+      string s;
+      if (!getline( ss, s, ',' )) break;
+      _word.appendSymbol(stoi(s));
+    }
+
+    if(_word.length() !=0) words.push_back(_word);
+  }
+
+  // for(int i = 0 ; i < words.size(); i++) {
+  //   if(words[i].length() == 0) cout << "{}" << endl;
+  //   else
+  //   cout << words[i].toString() << endl;
+  // }
+  cout << "SET SIZE:" << words.size() << endl;
+
+  return words;
+}
+
+void WordsGenerator::_saveWordsToFIle() {
+  fstream file;
+  file.open("./res/generated_words.txt",fstream::out);
+
+  //INFO
+  file << "#TRAINING SET CONST: " << _firstSubset.size() << endl;
+  file << "#TRAINING SET RAND: " << _secondSubset.size() <<endl;
+  file << "#TESTING SET: " << _testingSet.size() << endl << endl;
+
+  // Alphabet
+  file << "[ALPHABET]" << endl;
+  file << _alphabet.size() << endl << endl;
+
+  // First subset
+  file << "[TRAINING SET CONST]" << endl;
+  for(auto iter = _firstSubset.begin() ; iter != _firstSubset.end() ; ++iter) {
+    file << iter->toString() << endl;
+  }
+  file << endl;
+
+  // Second subset
+  file << "[TRAINING SET RAND]" << endl;
+  for(auto iter = _secondSubset.begin() ; iter != _secondSubset.end() ; ++iter) {
+    file << iter->toString() << endl;
+  }
+  file << endl;
+
+  // Testing subset
+  file << "[TESTING SET]" << endl;
+  for(auto iter = _testingSet.begin() ; iter != _testingSet.end() ; ++iter) {
+    file << iter->toString() << endl;
+  }
+
+  file.close();
+}
+
+void WordsGenerator::_calculateNumberOfWords() {
+  _wordsInFirstSubset = 0;
+    for(int i = 1; i <= _C; i++) {
+        _wordsInFirstSubset += pow(_alphabet.size(), i);
+        cout << _wordsInFirstSubset << endl;
+    }
+    _wordsInSecondSubset = _wordsInTrainingSet - _wordsInFirstSubset;
+}
+
 // Conditions specified in methodology
 void WordsGenerator::_checkGlobalConditions() {
+  if (_wordsInSecondSubset < _wordsInFirstSubset) {
+      throw invalid_argument("Too few words in training set.");
+  }
 
-    if (SIZE_S + SIZE_M + SIZE_L > R_MAX) {
-        throw invalid_argument("omega_s + omega_m + omega_l > r_max; check your flags.");
-    }
-
-    if (MAX_LENG_S >= MIN_LENG_M) {
-        throw invalid_argument("MAX_LENG_S > MIN_LENG_M; words from omega_S might be longer than those in omega_M");
-    }
-
-    if (MAX_LENG_M >= MIN_LENG_L) {
-        throw invalid_argument("MAX_LENG_M > MIN_LENG_L; words from omega_M might be longer than those in omega_L");
-    }
+  if(_wordsInTestingSet < _wordsInTrainingSet) {
+      throw invalid_argument("Less words in testing set than in training one");
+  }
 }
 
-void WordsGenerator::_fillBags() {
-    _fillBagWithWords(_omegaS, SIZE_S);
-    _fillBagWithWords(_omegaM, SIZE_M);
-    _fillBagWithWords(_omegaL, SIZE_L);
-    //_fillTestBag(R_MAX / 3);
-
-    std::stringstream ss;
-    ss << "Words Generated:" << std::endl;
-    ss << "Size Small:   " << SIZE_S << std::endl;
-    ss << "Size Medium:  " << SIZE_M << std::endl;
-    ss << "Size Large:   " << SIZE_L << std::endl;
-    ss << "Size Test:    " << R_MAX / 3;
-    logger::log(File("words.txt"), ss.str());
+void WordsGenerator::_generateWords() {
+  _firstSubset = _createAllWordsUpToLength(_C);
+  cout << "firstSubset created\n";
+  _secondSubset = _createRandomWordsOfLengthInInterval(_C+1, _maxWordLengthTraining, _wordsInSecondSubset);
+  cout << "secondSubset created\n";
+  _trainingSet = utils::concatenateVector(_firstSubset, _secondSubset);
+  cout << "trainingSet created\n";
+  _testingSet = _createRandomWordsOfLengthInInterval(_maxWordLengthTraining+1, _maxWordLengthTesting, _wordsInTestingSet);
+  cout << "testingSet created\n";
 }
 
-void WordsGenerator::_fillTestBag(int numberOfTestWords) {
-    // Produce as many alphabet words as words needed
-    for (int i = 0; i < numberOfTestWords; i++) {
-        int length = utils::generateRandomNumber(_testSet.getMinWordLength(), _testSet.getMaxWordLength());
-        Word word = _generateRandomWordOverAlphabet(length);
-        _testSet.addWord(word);
-    }
+// Creates all possible words of length from interval <0,N>.
+vector<Word> WordsGenerator::_createAllWordsUpToLength(int N) {
+  int alphabet_size = _alphabet.size();
+  vector<Word> _wordsList;
+
+	if(N == 0) return _wordsList;
+
+  _wordsList = _initBaseWords();
+
+	for(int n = 1; n < N; n++) {
+		vector<Word> _wordsOfSizeN = _getWordsOfSizeN(_wordsList, n);
+		for(unsigned int i = 0; i < _wordsOfSizeN.size(); i++) {
+				for(int a = 0; a < alphabet_size; a++) {
+						Word w = _wordsOfSizeN[i];
+						w.appendSymbol(a);
+						_wordsList.push_back(w);
+				}
+			}
+	}
+
+	return _wordsList;
 }
 
-void WordsGenerator::_fillBagWithWords(BagOfWords &bag, int numberOfWords) {
-    int alphabetSize = _alphabet.size();
-    bool moreWordsNeededThanSymbolsInAlphabet = numberOfWords > alphabetSize;
+vector<Word> WordsGenerator::_initBaseWords() {
+  vector<Word> output;
+  int alphabet_size = _alphabet.size();
+	for(int symbol = 0; symbol < alphabet_size; ++symbol) {
+		Word _base;
+		_base.appendSymbol(symbol);
+		output.push_back(_base);
+	}
+  return output;
+}
 
-    if (moreWordsNeededThanSymbolsInAlphabet) {
-
-        // Create alphabet based words
-        for (unsigned int symbol = 0; symbol <= _alphabet.size()-1; symbol++) {
-            int length = utils::generateRandomNumber(bag.getMinWordLength(), bag.getMaxWordLength());
-            Word word = _generateWordStartingWith(bag, symbol, length);
-            bag.addWord(word);
-        }
-
-        // Fill up rest of the space
-        int restOfTheSpaceSize = numberOfWords - _alphabet.size();
-        for (int i = 0; i < restOfTheSpaceSize; i++) {
-            int length = utils::generateRandomNumber(bag.getMinWordLength(), bag.getMaxWordLength());
-            Word word = _generateRandomWordOverAlphabet(length);
-            bag.addWord(word);
-        }
-
-    } else {
-        // Produce as many alphabet words as words needed
-        for (int symbol = 0; symbol <= numberOfWords-1; symbol++) {
-            int length = utils::generateRandomNumber(bag.getMinWordLength(), bag.getMaxWordLength());
-            Word word = _generateWordStartingWith(bag, symbol, length);
-            bag.addWord(word);
+vector<Word> WordsGenerator::_getWordsOfSizeN(vector<Word> _wordsList, int n) {
+    vector<Word> wordsOfSizeN;
+    for(unsigned int word = 0; word < _wordsList.size(); word++) {
+        Word w = _wordsList[word];
+        if(w.length() == n) {
+            wordsOfSizeN.push_back(w);
         }
     }
+    return wordsOfSizeN;
+}
+
+vector<Word> WordsGenerator::_createRandomWordsOfLengthInInterval(int min, int max, int count) {
+    vector<Word> randomWords;
+    for(int w = 0; w < count; w++) {
+      int length = utils::generateRandomNumber(min, max+1);
+      Word word = _generateRandomWordOverAlphabet(length);
+      randomWords.push_back(word);
+    }
+
+    return randomWords;
+
 }
 
 int WordsGenerator::hammingDistance(Word w1, Word w2) const {
@@ -121,17 +247,6 @@ int WordsGenerator::hammingDistance(Word w1, Word w2) const {
     return distance;
 }
 
-Word WordsGenerator::_generateWordStartingWith(BagOfWords &bag, int startingSymbol, int wordLength) {
-    Word word;
-    do {
-        int length = utils::generateRandomNumber(0, bag.getMaxWordLength());
-        word = _generateRandomWordOverAlphabet(length);
-        word[0] = startingSymbol;
-    } while (!_hammingConditionMet(word));
-
-    return word;
-}
-
 Word WordsGenerator::_generateRandomWordOverAlphabet(int length) {
     Word word;
 
@@ -144,156 +259,52 @@ Word WordsGenerator::_generateRandomWordOverAlphabet(int length) {
 }
 
 int WordsGenerator::_generateRandomSymbolFromAlphabet() {
-    unsigned int size = _alphabet.size();
-
     int firstSymbol = _alphabet[0];
-    int lastSymbol = _alphabet[size-1];
+    int lastSymbol = _alphabet.size();
 
     return utils::generateRandomNumber(firstSymbol, lastSymbol);
 }
 
-// Check if word meets conditions regarding those already created
-bool WordsGenerator::_hammingConditionMet(Word word) {
-    vector<Word> wordsInSpecificOmega;
-    int wordLength = word.length();
-
-    if (_omegaS.canWordBelongToTheBag(wordLength)) {
-        wordsInSpecificOmega = _omegaS.getWordsOfLength(word.length());
-    } else if (_omegaM.canWordBelongToTheBag(wordLength)) {
-        wordsInSpecificOmega = _omegaS.getWordsOfLength(word.length());
-    } else if (_omegaL.canWordBelongToTheBag(wordLength)) {
-        wordsInSpecificOmega = _omegaS.getWordsOfLength(word.length());
-    } else {
-        throw invalid_argument("word: " + word.toString()
-                               + " can not belong to any bag, because its length is to big.");
-    }
-
-    return _checkHammingCondition(word, wordsInSpecificOmega);
-}
-
-bool WordsGenerator::_checkHammingCondition(Word word, vector<Word> wordsToCompare) {
-
-    int acceptableHammingDistance = _calculateAcceptableHammingDistance(word.length());
-
-    if (acceptableHammingDistance < 1) acceptableHammingDistance = 1;
-
-    for (auto i = wordsToCompare.begin(); i != wordsToCompare.end(); ++i) {
-        int distance = hammingDistance(word, *i);
-
-        if (distance < acceptableHammingDistance) {
-            LOG_DEBUG("word: " + word.toString() + " and " + (*i).toString()
-                      + " have wrong hamming distance: " + to_string(distance) +
-                      " starting over ...");
-            return false;
-        }
-    }
-
-    return true;
-}
-
-
-int WordsGenerator::_calculateAcceptableHammingDistance(int length) {
-    return length / 2;
-}
-
-void WordsGenerator::_generatePairs() {
-    //vector<Word> allWords = _collectAllWordsFromBags();
-    _pairs = _combineIntoPairs(_psoWords);
-}
-
-void WordsGenerator::_generateTestPairs() {
-    vector<Word> allTestWords = _testSet.getAllWords();
-    _testPairs = _combineIntoPairs(allTestWords);
-}
-
-vector<Word> WordsGenerator::_collectAllWordsFromBags() {
-    vector<Word> wordsOmegaS = _omegaS.getAllWords();
-    vector<Word> wordsOmegaM = _omegaM.getAllWords();
-    vector<Word> wordsOmegaL = _omegaL.getAllWords();
-    vector<Word> allWords = utils::mergeVectors(wordsOmegaS, wordsOmegaM, wordsOmegaL);
-
-    return allWords;
-}
-
 vector<PairOfWords> WordsGenerator::_combineIntoPairs(vector<Word> words) {
     vector<PairOfWords> pairs;
-    for (unsigned int i = 0; i < words.size() - 1; i++) {
-        for (unsigned int j = i; j < words.size(); j++) {
-            PairOfWords pairOfWords(words[i], words[j]);
-            pairs.push_back(pairOfWords);
-        }
-    }
+    // for (unsigned int i = 0; i < words.size() - 1; i++) {
+    //     for (unsigned int j = i; j < words.size(); j++) {
+    //         PairOfWords pairOfWords(words[i], words[j]);
+    //         pairs.push_back(pairOfWords);
+    //     }
+    // }
     return pairs;
 }
 
-vector<PairOfWords> *WordsGenerator::getPairs() {
-    return &_pairs;
+vector<PairOfWords>* WordsGenerator::getPairs() {
+    return NULL;
 }
 
-vector<PairOfWords> *WordsGenerator::getTestPairs() {
-    return &_testPairs;
+vector<PairOfWords>* WordsGenerator::getTestPairs() {
+    return NULL;
 }
 
 void WordsGenerator::print() {
-    cout << "OMEG_S:\n";
-    _omegaS.print();
-    cout << "OMEG_M:\n";
-    _omegaM.print();
-    cout << "OMEG_L:\n";
-    _omegaL.print();
-}
+  cout << "[TRAINING SET]" << endl;
 
-/////////// NEW SET OF FUNCTIONS //////////////////
+  for(auto iter = _trainingSet.begin(); iter != _trainingSet.end(); ++iter) {
+    cout << (*iter).toString() << endl;
+  }
 
-void WordsGenerator::_createAllWordsUpToLength(int N) {
-	
-	// Prepare local copy of the alphabet
-	vector<int> local_alph_cp;
-	local_alph_cp.reserve(_alphabet.size());
-	copy(_alphabet.begin(),_alphabet.end(),back_inserter(local_alph_cp));
-	
-	// Create consecutive permutations and save them
-	sort(local_alph_cp.begin(), local_alph_cp.end());
-	do {
-		Word _w(local_alph_cp);
-		_psoWords.push_back(_w);
-	} while (next_permutation(local_alph_cp.begin(), local_alph_cp.end()));	
-  	
-  	//for(unsigned int i = 0; i < _psoWords.size() ; i++) {
-	//	cout << _psoWords[i].toString() << endl;
-	//}
-	 	
-}
 
-void WordsGenerator::printAllKLength(vector<int> alphabet, int k) {
-        int n = alphabet.size();  
-        Word word;      
-        printAllKLengthRec(alphabet, word, n, k);
-}
- 
-// The main recursive method to print all possible strings of length k
-void WordsGenerator::printAllKLengthRec(vector<int> alphabet, Word word, int n, int k) {
-	 
-	// Base case: k is 0, print prefix
-	if (k == 0) {
-		//cout << word.toString();
-		_psoWords.push_back(word);
-		return;
-	}
+  cout << "[TESTING SET]" << endl;
 
-	// One by one add all characters from set and recursively 
-	// call for k equals to k-1
-	for (int i = 0; i < n; ++i) {
-		 //cout << i << endl;
-		// Next character of input added
-		word.appendSymbol(alphabet[i]);
-		Word new_prefix(word);
-	
-		// k is decreased, because we have added a new character
-		printAllKLengthRec(alphabet, new_prefix, n, k - 1); 
-	}
+  for(auto iter = _testingSet.begin(); iter != _testingSet.end(); ++iter) {
+    cout << (*iter).toString() << endl;
+  }
 }
 
 
-
-
+void WordsGenerator::_printInfo() {
+  std::stringstream ss;
+  // ss << "Words Generated:" << std::endl;
+  // ss << "All up to length:   " << R_MAX << std::endl;
+  // ss << "Number of words:    " << _psoWords.size() << endl;
+  // ss << "Size Test:    	   " << SIZE_S;
+  logger::log(File("words.txt"), ss.str());
+}
